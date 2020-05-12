889c9ae60b6bafc38da6df1a68c59b31bff1add7
dinabogdan <bogdan.dina03@gmail.com>
2020-05-12 15:59:03 +0300

    more types

--- src/main/kotlin/com/freesoft/joykt/chapter3/Types.kt
+++ src/main/kotlin/com/freesoft/joykt/chapter3/Types.kt
@@ -1,13 +1,36 @@
 package com.freesoft.joykt.chapter3
 
+import java.lang.IllegalArgumentException
+
 data class Price(val value: Double) {
+
+    override fun toString() = value.toString()
+
     operator fun plus(price: Price) = Price(this.value + price.value)
     operator fun times(num: Int) = Price(this.value * num)
+
+    companion object {
+        val identity = Price(0.0)
+
+        operator fun invoke(value: Double) = if (value > 0)
+            Price(value)
+        else
+            throw IllegalArgumentException("Price must be positive or null")
+    }
 }
 
 data class Weight(val value: Double) {
     operator fun plus(weight: Weight) = Weight(this.value + weight.value)
     operator fun times(num: Int) = Weight(this.value * num)
+
+    companion object {
+        val identity = Weight(0.0)
+
+        operator fun invoke(value: Double) = if (value > 0)
+            Weight(value)
+        else
+            throw IllegalArgumentException("Weight must be positive or null")
+    }
 }
 
 data class Product(
@@ -28,19 +51,16 @@ object Store {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        val toothPaste = Product("Tooth paste", 1.5, 0.5)
-        val toothBrush = Product("Tooth brush", 3.5, 0.3)
+        val toothPaste = Product("Tooth paste", Price(1.5), Weight(0.5))
+        val toothBrush = Product("Tooth brush", Price(3.5), Weight(0.3))
 
         val orderLines = listOf(
                 OrderLine(toothPaste, 2),
                 OrderLine(toothBrush, 3)
         )
 
-        val zeroPrice = Price(0.0)
-        val zeroWeight = Weight(0.0)
-        
-        val weight: Weight = orderLines.fold(zeroWeight) { a, b -> a + b.weight() }
-        val price: Price = orderLines.fold(zeroPrice) { a, b -> a + b.amount() }
+        val weight: Weight = orderLines.fold(Weight.identity) { a, b -> a + b.weight() }
+        val price: Price = orderLines.fold(Price.identity) { a, b -> a + b.amount() }
 
         println("Total price: $price")
         println("Total weight: $weight")
