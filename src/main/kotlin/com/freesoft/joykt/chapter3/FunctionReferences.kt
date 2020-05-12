b1eba9a3e569cbba09ea701f57be2efbd55c8835
dinabogdan <bogdan.dina03@gmail.com>
2020-05-12 10:22:29 +0300

    currying

--- src/main/kotlin/com/freesoft/joykt/chapter3/FunctionReferences.kt
+++ src/main/kotlin/com/freesoft/joykt/chapter3/FunctionReferences.kt
@@ -13,7 +13,18 @@ fun triple(n: Int) = n * 3
 
 fun <T, U, V> compose(f: (U) -> V, g: (T) -> U): (T) -> V = { x -> f(g(x)) }
 
+fun add(n: Int): (Int) -> Int = { x -> x + n }
+
+typealias IntBinOp = (Int) -> (Int) -> Int
+
+val add2: IntBinOp = { a -> { b -> a + b } }
+val multiply: IntBinOp = { a -> { b -> a * b } }
+
 fun main() {
     val squareOfTriple = compose(::square, ::triple)
     println(squareOfTriple(5))
+
+    println(add(5)(4))
+    println(add2(6)(10))
+    println(multiply(4)(5))
 }
