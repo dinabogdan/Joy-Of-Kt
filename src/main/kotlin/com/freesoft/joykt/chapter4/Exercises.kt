05ae9d0d8456b480a00cc261b4228a993fe04df1
dinabogdan <bogdan.dina03@gmail.com>
2020-05-13 14:53:45 +0300

    recursive value functions

--- src/main/kotlin/com/freesoft/joykt/chapter4/Exercises.kt
+++ src/main/kotlin/com/freesoft/joykt/chapter4/Exercises.kt
@@ -8,6 +8,21 @@ tailrec fun add(a: Int, b: Int): Int {
     else add(dec(a), inc(b))
 }
 
+fun factorial(n: Int): Int = if (n == 0) 1 else n * factorial(n - 1)
+
+object Factorial {
+    private lateinit var fact: (Int) -> Int
+
+    init {
+        fact = { n -> if (n <= 1) n else n * fact(n - 1) }
+    }
+
+    val factorial = fact
+}
+
 fun main() {
     println(add(5, 6))
+
+    println(factorial(10))
+    println(Factorial.factorial(10))
 }
