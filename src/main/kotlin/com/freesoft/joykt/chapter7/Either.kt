b32976ca785aeb46850812a543e04dbfaa2b8948
dinabogdan <bogdan.dina03@gmail.com>
2020-05-17 17:23:14 +0300

    add map and flatMap

--- src/main/kotlin/com/freesoft/joykt/chapter7/Either.kt
+++ src/main/kotlin/com/freesoft/joykt/chapter7/Either.kt
@@ -4,12 +4,24 @@ import com.freesoft.joykt.chapter5.List
 
 sealed class Either<out A, out B> {
 
+    abstract fun <C> map(f: (B) -> C): Either<A, C>
+
+    abstract fun <C> flatMap(f: (B) -> Either<@UnsafeVariance A, C>): Either<A, C>
+
     internal class Left<out A, out B>(private val value: A) : Either<A, B>() {
         override fun toString(): String = "Left($value)"
+
+        override fun <C> map(f: (B) -> C): Either<A, C> = Left(value)
+
+        override fun <C> flatMap(f: (B) -> Either<@UnsafeVariance A, C>): Either<A, C> = Left(value)
     }
 
     internal class Right<out A, out B>(private val value: B) : Either<A, B>() {
         override fun toString(): String = "Right($value)"
+
+        override fun <C> map(f: (B) -> C): Either<A, C> = Right(f(value))
+
+        override fun <C> flatMap(f: (B) -> Either<@UnsafeVariance A, C>): Either<A, C> = f(value)
     }
 
     companion object {
@@ -22,7 +34,7 @@ fun <A : Comparable<A>> max(list: List<A>): Either<String, A> = when (list) {
     is List.Nil -> Either.left("max called on an empty list")
     is List.Cons -> Either.right(list.foldLeft(list.head) { x ->
         { y ->
-            if (x.compareTo(y) == -) x else y
+            if (x.compareTo(y) == 0) x else y
         }
     })
 }
