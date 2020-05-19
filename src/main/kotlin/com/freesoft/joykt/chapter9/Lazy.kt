ce521a9d061ae0a6c448d400dbb67a017e2c9b57
dinabogdan <bogdan.dina03@gmail.com>
2020-05-19 14:47:18 +0300

    lazily vs eagerly evaluation

--- src/main/kotlin/com/freesoft/joykt/chapter9/Lazy.kt
+++ src/main/kotlin/com/freesoft/joykt/chapter9/Lazy.kt
@@ -1,14 +1,41 @@
 package com.freesoft.joykt.chapter9
 
 import java.lang.IllegalStateException
+import kotlin.random.Random
 
 class Lazy<out A>(function: () -> A) : () -> A {
     private val value: A by lazy(function)
 
     override operator fun invoke(): A = value
+
+    override fun toString(): String = "$value"
 }
 
 fun or(a: Lazy<Boolean>, b: Lazy<Boolean>): Boolean = if (a()) true else b()
+
+fun constructMessage(greetings: String, name: String): String = "$greetings, $name!"
+
+fun lazyConstructMessage(greetings: Lazy<String>, name: Lazy<String>): Lazy<String> = Lazy { "${greetings()}, ${name()}!" }
+
+fun greetings(): String {
+    println("Eager Evaluating greetings")
+    return "Hello"
+}
+
+fun name(): String {
+    println("Eager computing name")
+    return "Mickey"
+}
+
+val lazyGreetings = Lazy {
+    println("Lazy Evaluating greetings")
+    "Hello"
+}
+
+val lazyName: Lazy<String> = Lazy {
+    println("Lazy computing name")
+    "Mickey"
+}
 
 fun main() {
 
@@ -25,5 +52,15 @@ fun main() {
     println(first() || second())
     println(first() || second())
     println(or(first, second))
+
+    val lazyMessage = lazyConstructMessage(lazyGreetings, lazyName)
+    val eagerMessage = constructMessage(greetings(), name())
+    val condition = Random(System.currentTimeMillis()).nextInt() % 2 == 0
+
+    // the following println() will call the greetings() and name() function even if the condition is not true
+    println(if (condition) println(eagerMessage) else "No greetings when time is odd")
+
+    // the following println() will not evaluate the lazyGreetings and lazyName if the condition is not true
+    println(if (condition) println(lazyMessage) else "No greetings when time is odd")
 
 }
