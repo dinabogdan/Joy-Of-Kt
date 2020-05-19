9508fc3ca5daca73106f60f3162ef8177f67c294
dinabogdan <bogdan.dina03@gmail.com>
2020-05-19 22:49:51 +0300

    add dropAtMost and takeAtMost

--- src/main/kotlin/com/freesoft/joykt/chapter9/Stream.kt
+++ src/main/kotlin/com/freesoft/joykt/chapter9/Stream.kt
@@ -6,6 +6,10 @@ sealed class Stream<out A> {
     abstract fun isEmpty(): Boolean
     abstract fun head(): Result<A>
     abstract fun tail(): Result<Stream<A>>
+    abstract fun takeAtMost(n: Int): Stream<A>
+    abstract fun dropAtMost(n: Int): Stream<A>
+
+    fun <A> repeat(f: () -> A): Stream<A> = cons(Lazy { f() }, Lazy { repeat(f) })
 
     private object Empty : Stream<Nothing>() {
         override fun isEmpty(): Boolean = true
@@ -14,6 +18,9 @@ sealed class Stream<out A> {
 
         override fun tail(): Result<Stream<Nothing>> = Result()
 
+        override fun takeAtMost(n: Int): Stream<Nothing> = this
+
+        override fun dropAtMost(n: Int): Stream<Nothing> = this
     }
 
     private class Cons<out A>(
@@ -25,6 +32,13 @@ sealed class Stream<out A> {
         override fun head(): Result<A> = Result(_head())
 
         override fun tail(): Result<Stream<A>> = Result(_tail())
+
+        override fun takeAtMost(n: Int): Stream<A> = when {
+            n > 0 -> cons(_head, Lazy { _tail().takeAtMost(n - 1) })
+            else -> Empty
+        }
+
+        override fun dropAtMost(n: Int): Stream<A> = Companion.dropAtMost(n, this)
     }
 
     companion object {
@@ -34,6 +48,14 @@ sealed class Stream<out A> {
         operator fun <A> invoke(): Stream<A> = Empty
 
         fun from(i: Int): Stream<Int> = cons(Lazy { i }, Lazy { from(i + 1) })
+
+        tailrec fun <A> dropAtMost(n: Int, stream: Stream<A>): Stream<A> = when {
+            n > 0 -> when (stream) {
+                is Empty -> stream
+                is Cons -> dropAtMost(n - 1, stream._tail())
+            }
+            else -> stream
+        }
     }
 }
 
