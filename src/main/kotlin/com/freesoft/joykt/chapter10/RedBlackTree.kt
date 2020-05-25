07e4e60ae4da12f60d5b9bc1d2ab72fa4c8842c7
dinabogdan <bogdan.dina03@gmail.com>
2020-05-25 22:36:46 +0300

    add balance function

--- src/main/kotlin/com/freesoft/joykt/chapter10/RedBlackTree.kt
+++ src/main/kotlin/com/freesoft/joykt/chapter10/RedBlackTree.kt
@@ -16,6 +16,26 @@ sealed class RedBlackTree<out A : Comparable<@kotlin.UnsafeVariance A>> {
     internal abstract val left: RedBlackTree<A>
     internal abstract val value: A
 
+    internal abstract fun blacken(): RedBlackTree<A>
+
+    internal abstract fun add(newVal: @UnsafeVariance A): RedBlackTree<A>
+
+    protected fun balance(color: Color,
+                          left: RedBlackTree<@UnsafeVariance A>,
+                          value: @UnsafeVariance A,
+                          right: RedBlackTree<@UnsafeVariance A>
+    ): RedBlackTree<A> = when {
+        color == Color.B && left.isTR && left.left.isTR ->
+            T(Color.R, left.left.blacken(), left.value, T(Color.B, left.right, value, right))
+        color == Color.B && left.isTR && left.right.isTR ->
+            T(Color.R, T(Color.B, left.left, left.value, left.right.left), left.right.value, T(Color.B, left.right.right, value, right))
+        color == Color.B && right.isTR && right.left.isTR ->
+            T(Color.R, T(Color.B, left, value, right.left.left), right.left.value, T(Color.B, right.left.right, right.value, right.right))
+        color == Color.B && right.isTR && right.right.isTR ->
+            T(Color.R, T(Color.B, left, value, right.left), right.value, right.right.blacken())
+        else -> T(color, left, value, right)
+    }
+
     internal abstract class Empty<out A : Comparable<@kotlin.UnsafeVariance A>> : RedBlackTree<A>() {
         override val isTB: Boolean = false
         override val isTR: Boolean = false
@@ -37,6 +57,12 @@ sealed class RedBlackTree<out A : Comparable<@kotlin.UnsafeVariance A>> {
         override val color: Color = Color.B
 
         override fun toString(): String = "E"
+
+        override fun blacken(): RedBlackTree<Nothing> = E
+
+        override fun add(newVal: @UnsafeVariance A): RedBlackTree<A> = T(Color.R, E, newVal, E)
+
+        operator fun plus(value: @UnsafeVariance A): RedBlackTree<A> = add(value).blacken()
     }
 
     internal object E : Empty<Nothing>()
@@ -55,6 +81,17 @@ sealed class RedBlackTree<out A : Comparable<@kotlin.UnsafeVariance A>> {
         override val isTR: Boolean = color == Color.R
 
         override fun toString(): String = "(T $color $left $value $right)"
+
+        override fun blacken(): RedBlackTree<A> = T(Color.B, left, value, right)
+
+        override fun add(newVal: @UnsafeVariance A): RedBlackTree<A> = when {
+            newVal < value -> balance(color, left.add(newVal), value, right)
+            newVal > value -> balance(color, left, value, right.add(newVal))
+            else -> when (color) {
+                Color.B -> T(Color.B, left, newVal, right)
+                Color.R -> T(Color.R, left, newVal, right)
+            }
+        }
     }
 
     companion object {
