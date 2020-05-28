972c50efdc93e12a2587c3dd2b5412d3f4e1e4e4
dinabogdan <bogdan.dina03@gmail.com>
2020-05-28 09:38:43 +0300

    trampolining into IO type

--- src/main/kotlin/com/freesoft/joykt/chapter12/IO.kt
+++ src/main/kotlin/com/freesoft/joykt/chapter12/IO.kt
@@ -10,53 +10,114 @@ import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
 
-class IO<out A>(private val effect: () -> A) {
+sealed class IO<out A> {
 
-    fun <B> map(g: (A) -> B): IO<B> = IO { g(this()) }
+    internal class Return<out A>(val value: A) : IO<A>()
 
-    fun <B> flatMap(g: (A) -> IO<B>): IO<B> = IO {
-        g(this())()
+    internal class Suspend<out A>(val resume: () -> A) : IO<A>()
+
+    internal class Continue<A, out B>(val sub: IO<A>, val f: (A) -> IO<B>) : IO<A>()
+
+    fun <B> map(g: (A) -> B): IO<B> = flatMap { Return(g(it)) }
+
+    fun <B> flatMap(g: (A) -> IO<B>): IO<B> = Continue(this, g) as IO<B>
+
+    class IORef<A>(private var value: A) {
+        fun set(a: A): IO<A> {
+            value = a
+            return unit(a)
+        }
+
+        fun get(): IO<A> = unit(value)
+
+        fun modify(f: (A) -> A): IO<A> = get().flatMap { a -> set(f(a)) }
     }
 
-    operator fun invoke() = effect()
+    operator fun invoke(): A = invoke(this)
 
-    operator fun plus(io: IO<@UnsafeVariance A>): IO<A> = IO {
-        effect()
-        io.effect()
+    operator fun invoke(io: IO<@UnsafeVariance A>): A {
+        tailrec fun invokeHelper(io: IO<A>): A =
+                when (io) {
+                    is Return -> io.value
+                    is Suspend -> io.resume()
+                    else -> {
+                        val ct = io as Continue<A, A>
+                        val sub = ct.sub
+                        val f = ct.f
+                        when (sub) {
+                            is Return -> invokeHelper(f(sub.value))
+                            is Suspend -> invokeHelper(f(sub.resume()))
+                            else -> {
+                                val ct2 = sub as Continue<A, A>
+                                val sub2 = ct2.sub
+                                val f2 = ct2.f
+                                invokeHelper(sub2.flatMap { f2(it).flatMap(f) })
+                            }
+                        }
+                    }
+                }
+        return invokeHelper(io)
     }
 
     companion object {
-        val empty: IO<Unit> = IO {}
+        val empty: IO<Unit> = IO.Suspend { Unit }
+
+        internal fun <A> unit(a: A): IO<A> = IO.Suspend { a }
 
         fun <A, B, C> map2(ioA: IO<A>, ioB: IO<B>, f: (A) -> (B) -> C): IO<C> = ioA.flatMap { a ->
             ioB.map { b -> f(a)(b) }
         }
 
-        fun <A> repeat(n: Int, io: IO<A>): IO<List<A>> {
-            val stream: Stream<IO<A>> = Stream.fill(n, Lazy { io })
-            val f: (A) -> (List<A>) -> List<A> = { a ->
-                { la: List<A> -> List.Cons(a, la) }
-            }
-            val g: (IO<A>) -> (Lazy<IO<List<A>>>) -> IO<List<A>> =
-                    { ioa ->
-                        { sioLa ->
-                            map2(ioa, sioLa(), f)
-                        }
-                    }
-            val z: Lazy<IO<List<A>>> = Lazy { IO { List<A>() } }
-            return stream.foldRight(z, g)
+//        fun <A> repeat(n: Int, io: IO<A>): IO<List<A>> {
+//            val stream: Stream<IO<A>> = Stream.fill(n, Lazy { io })
+//            val f: (A) -> (List<A>) -> List<A> = { a ->
+//                { la: List<A> -> List.Cons(a, la) }
+//            }
+//            val g: (IO<A>) -> (Lazy<IO<List<A>>>) -> IO<List<A>> =
+//                    { ioa ->
+//                        { sioLa ->
+//                            map2(ioa, sioLa(), f)
+//                        }
+//                    }
+//            val z: Lazy<IO<List<A>>> = Lazy { IO { List<A>() } }
+//            return stream.foldRight(z, g)
+//        }
+
+        fun <A, B> forever(ioA: IO<A>): IO<B> {
+            val t: () -> IO<B> = { forever(ioA) }
+            return ioA.flatMap { t() }
         }
 
 
-        operator fun <A> invoke(a: A): IO<A> = IO { a }
+//        operator fun <A> invoke(a: A): IO<A> = IO.Suspend { a }
     }
 
 }
 
+//object Console {
+//    private val br = BufferedReader(InputStreamReader(System.`in`))
+//
+//    fun readln(): IO<Result<String>> = IO {
+//        try {
+//            Result(br.readLine())
+//        } catch (ex: IOException) {
+//            Result.failure<String>(ex)
+//        }
+//    }
+//
+//    fun println(o: Any): IO<Unit> = IO {
+//        kotlin.io.println(o.toString())
+//    }
+//
+//    fun print(o: Any): IO<Unit> = IO {
+//        kotlin.io.print(o.toString())
+//    }
+//}
+
 object Console {
     private val br = BufferedReader(InputStreamReader(System.`in`))
 
-    fun readln(): IO<Result<String>> = IO {
+    fun readLine(): IO<Result<String>> = IO.Suspend {
         try {
             Result(br.readLine())
         } catch (ex: IOException) {
@@ -64,16 +125,12 @@ object Console {
         }
     }
 
-    fun println(o: Any): IO<Unit> = IO {
-        kotlin.io.println(o.toString())
-    }
+    fun printLine(s: Any): IO<Unit> = IO.Suspend { println(s) }
 
-    fun print(o: Any): IO<Unit> = IO {
-        kotlin.io.print(o.toString())
-    }
+    fun print(s: Any): IO<Unit> = IO.Suspend { print(s) }
 }
 
-fun show(message: String): IO<Unit> = IO { println(message) }
+fun show(message: String): IO<Unit> = Console.printLine(message)
 
 fun <A> toString(rd: Result<A>): String = rd.map { it.toString() }.getOrElse(rd.toString())
 
@@ -86,21 +143,21 @@ val computation: IO<Unit> = show(toString(inverse(3)))
 
 fun getName() = "Mickey"
 
-val instruction1 = IO { print("Hello, ") }
-val instruction2 = IO { print(getName()) }
-val instruction3 = IO { print("!\n") }
+//val instruction1 = IO { print("Hello, ") }
+//val instruction2 = IO { print(getName()) }
+//val instruction3 = IO { print("!\n") }
 
-val script: IO<Unit> = instruction1 + instruction2 + instruction3
+//val script: IO<Unit> = instruction1 + instruction2 + instruction3
 
 private fun sayHello(): IO<Unit> = Console.print("Enter your name: ")
-        .map { Console.readln()() }
+        .map { Console.readLine()() }
         .map { "Hello, ${it.getOrElse("")}!" }
-        .map { Console.println(it)() }
+        .map { Console.printLine(it)() }
 
 private fun sayFunctionalHellO(): IO<Unit> = Console.print("Enter your name: ")
-        .flatMap { Console.readln() }
+        .flatMap { Console.readLine() }
         .map { "Hello, ${it.getOrElse("")}!" }
-        .flatMap { Console.println(it) }
+        .flatMap { Console.printLine(it) }
 
 fun main() {
 //    script()
@@ -117,9 +174,14 @@ fun main() {
 
 //    val script = sayHello()
 
-    val script = sayFunctionalHellO()
+//    val script = sayFunctionalHellO()
+//
+//    script()
 
-    script()
+//    val program = IO.forever<String, String>(IO { "Hi again!!" })
+//            .flatMap { Console.println(it) }
+//
+//    program()
 
 
 }
