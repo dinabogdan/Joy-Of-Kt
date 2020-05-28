29ef250f1244abf81f646dbc8ed733d2415c73a6
dinabogdan <bogdan.dina03@gmail.com>
2020-05-28 08:34:04 +0300

    make IO type to handle input

--- src/main/kotlin/com/freesoft/joykt/chapter12/IO.kt
+++ src/main/kotlin/com/freesoft/joykt/chapter12/IO.kt
@@ -3,21 +3,23 @@ package com.freesoft.joykt.chapter12
 import com.freesoft.joykt.chapter5.List
 import com.freesoft.joykt.chapter7.Result
 
-class IO(private val effect: () -> Unit) {
+class IO<out A>(private val effect: () -> A) {
     operator fun invoke() = effect()
 
-    operator fun plus(io: IO): IO = IO {
+    operator fun plus(io: IO<@UnsafeVariance A>): IO<A> = IO {
         effect()
         io.effect()
     }
 
     companion object {
-        val empty: IO = IO {}
+        val empty: IO<Unit> = IO {}
+
+        operator fun <A> invoke(a: A): IO<A> = IO { a }
     }
 
 }
 
-fun show(message: String): IO = IO { println(message) }
+fun show(message: String): IO<Unit> = IO { println(message) }
 
 fun <A> toString(rd: Result<A>): String = rd.map { it.toString() }.getOrElse(rd.toString())
 
@@ -26,7 +28,7 @@ fun inverse(i: Int): Result<Double> = when (i) {
     else -> Result(1.0 / i)
 }
 
-val computation: IO = show(toString(inverse(3)))
+val computation: IO<Unit> = show(toString(inverse(3)))
 
 fun getName() = "Mickey"
 
@@ -34,7 +36,7 @@ val instruction1 = IO { print("Hello, ") }
 val instruction2 = IO { print(getName()) }
 val instruction3 = IO { print("!\n") }
 
-val script: IO = instruction1 + instruction2 + instruction3
+val script: IO<Unit> = instruction1 + instruction2 + instruction3
 
 fun main() {
     script()
