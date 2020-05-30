d2811ba0461ebbfc9648d6e99a12c6b14570a9e2
dinabogdan <bogdan.dina03@gmail.com>
2020-05-30 14:58:47 +0300

    reading enum types

--- src/main/kotlin/com/freesoft/joykt/chapter14/ReadingFiles.kt
+++ src/main/kotlin/com/freesoft/joykt/chapter14/ReadingFiles.kt
@@ -3,12 +3,13 @@ package com.freesoft.joykt.chapter14
 import java.util.*
 import com.freesoft.joykt.chapter7.Result
 import java.io.IOException
+import java.lang.NumberFormatException
 import java.lang.invoke.MethodHandles
 
 class PropertyReader(configFileName: String) {
 
 
-    // this code works, but it relies on Java reading file mechanism which can throw exceptions
+// this code works, but it relies on Java reading file mechanism which can throw exceptions
 //    internal val properties: Result<Properties> =
 //            Result.of {
 //                MethodHandles.lookup()
@@ -43,23 +44,54 @@ class PropertyReader(configFileName: String) {
                 Result.failure("Exception: ${e.message} while reading classpath resource $configFileName")
             }
 
-
-    fun readProperty(name: String) = properties.flatMap {
+    fun readAsString(name: String) = properties.flatMap {
         Result.of {
             it.getProperty(name)
         }.mapFailure("Property $name not found")
     }
+
+    fun readAsInt(name: String): Result<Int> = readAsString(name).flatMap {
+        try {
+            Result(it.toInt())
+        } catch (e: NumberFormatException) {
+            Result.failure<Int>("Invalid value while parsing property '$name' to Int: '$it'")
+        }
+    }
+
+    fun <T> readAsType(f: (String) -> Result<T>, name: String) =
+            readAsString(name).flatMap {
+                try {
+                    f(it)
+                } catch (ex: Exception) {
+                    Result.failure<T>(
+                            "Invalid value while parsing property '$name': '$it'"
+                    )
+                }
+            }
+
+    inline fun <reified T : Enum<T>> readAsEnum(name: String, enumClass: Class<T>): Result<T> {
+        val f: (String) -> Result<T> = {
+            try {
+                val value = enumValueOf<T>(it)
+                Result(value)
+            } catch (ex: Exception) {
+                Result.failure("Error parsing property: '$name' value '$it' can't be parsed to ${enumClass.name}")
+            }
+        }
+        return readAsType(f, name)
+    }
 }
+
+enum class Type { SERIAL, PARALLEL }
 
 fun main() {
     val propertyReader = PropertyReader("/config.properties")
 
-    val person = propertyReader.readProperty("id")
-            .map { s -> s.toInt() }
+    val person = propertyReader.readAsInt("id")
             .flatMap { id ->
-                propertyReader.readProperty("firstName")
+                propertyReader.readAsString("firstName")
                         .flatMap { firstName ->
-                            propertyReader.readProperty("lastName")
+                            propertyReader.readAsString("lastName")
                                     .map { lastName -> Person.of(id, firstName, lastName) }
                         }
             }
@@ -69,23 +101,26 @@ fun main() {
             onFailure = { println(it) }
     )
 
-    propertyReader.readProperty("host")
+    propertyReader.readAsString("host")
             .forEach(
                     onSuccess = { println(it) },
                     onFailure = { println(it) }
             )
 
-    propertyReader.readProperty("name")
+    propertyReader.readAsString("name")
             .forEach(
                     onSuccess = { println(it) },
                     onFailure = { println(it) }
             )
 
-    propertyReader.readProperty("year")
+    propertyReader.readAsString("year")
             .forEach(
                     onSuccess = { println(it) },
                     onFailure = { println(it) }
             )
+
+    val type = propertyReader.readAsEnum("type", Type::class.java)
+    println(type)
 
 
 //    propertyReader.properties.forEach(
